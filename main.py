3b461620422a8b0e04ea40fcd78460e0fe8654fc
UniUrchin <85057870+UniUrchin@users.noreply.github.com>
2022-12-03 04:11:08 +0900

    ぺんぎんの動作を追加

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class PyxelText:
   def print(self):
     pyxel.text(self.pos_x, self.pos_y, self.word, self.color)
 
-class PyxelDoubledText(PyxelText) :
+class PyxelDoubledText(PyxelText):
   def __init__(self, pos_x, pos_y, word, main_color, shadow_color):
     self.pos_x = pos_x
     self.pos_y = pos_y
@@ -52,8 +52,23 @@ class PyxelImage:
     self.height = height
 
   def print(self):
-    pyxel.blt(self.pos_x, self.pos_y, self.imgnum, self.pos_u, self.pos_v, self.width, self.height)
+    pyxel.blt(self.pos_x, self.pos_y, self.imgnum, self.pos_u, self.pos_v, self.width, self.height, BG_COLOR)
 
+class Penguin(PyxelImage):
+  def __init__(self, pos_x, pos_y, pos_u, pos_v):
+    self.pos_x = pos_x
+    self.pos_y = pos_y
+    self.imgnum = 0
+    self.pos_u = pos_u
+    self.pos_v = pos_v
+    self.width = 16
+    self.height = 16
+
+  def walk_right(self):
+    return Penguin(self.pos_x + 1, self.pos_y, pyxel.frame_count % 4 * 16, 0)
+ 
+  def walk_left(self):
+    return Penguin(self.pos_x - 1, self.pos_y, pyxel.frame_count % 4 * 16, 16)
 
 class App:
   def __init__(self):
@@ -62,6 +77,7 @@ class App:
 
     self.title = PyxelImage(0, 0, 1, 128, 0, 128, 128)
     self.score = PyxelDoubledText(30, 4, "35", COLOR_ORANGE, COLOR_BROWN)
+    self.penguin = Penguin(64, 106, 0, 0)
 
     pyxel.run(self.update, self.draw)
 
@@ -69,10 +85,16 @@ class App:
     if pyxel.btnp(pyxel.KEY_Q):
       pyxel.quit()
 
+    if pyxel.btn(pyxel.KEY_LEFT):
+      self.penguin = self.penguin.walk_left()
+    if pyxel.btn(pyxel.KEY_RIGHT):
+      self.penguin = self.penguin.walk_right()
+
   def draw(self):
     pyxel.cls(BG_COLOR)
     self.title.print()
     self.score.print()
+    self.penguin.print()
 
 if __name__ == "__main__":
   App()
