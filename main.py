1963f296eb8d2439dc7626f22ea1a9a099734aa4
UniUrchin <85057870+UniUrchin@users.noreply.github.com>
2022-12-03 03:22:27 +0900

    タイトルとステージのpyxresを描いた

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import pyxel
 
-BG_COLOR = 7
+COLOR_BLACK      = 0
+COLOR_NAVY       = 1
+COLOR_PURPLE     = 2
+COLOR_GREEN      = 3
+COLOR_BROWN      = 4
+COLOR_DARK_BLUE  = 5
+COLOR_LIGHT_BLUE = 6
+COLOR_WHITE      = 7
+COLOR_RED        = 8
+COLOR_ORANGE     = 9
+COLOR_YELLOW     = 10
+COLOR_LIME       = 11
+COLOR_CYAN       = 12
+COLOR_GRAY       = 13
+COLOR_PINK       = 14
+COLOR_PEACH      = 15
+
+BG_COLOR = COLOR_PEACH
 
 class PyxelText:
   def __init__(self, pos_x, pos_y, word, color):
@@ -12,9 +29,17 @@ class PyxelText:
   def print(self):
     pyxel.text(self.pos_x, self.pos_y, self.word, self.color)
 
-class PyxelRainbowText(PyxelText):
-  def roop_color(self):
-    return PyxelRainbowText(self.pos_x, self.pos_y, self.word, pyxel.frame_count % 16)
+class PyxelDoubledText(PyxelText) :
+  def __init__(self, pos_x, pos_y, word, main_color, shadow_color):
+    self.pos_x = pos_x
+    self.pos_y = pos_y
+    self.word = word
+    self.main_color = main_color
+    self.shadow_color = shadow_color
+
+  def print(self):
+    pyxel.text(self.pos_x + 1, self.pos_y, self.word, self.shadow_color)
+    pyxel.text(self.pos_x, self.pos_y, self.word, self.main_color)
 
 class PyxelImage:
   def __init__(self, pos_x, pos_y, imgnum, pos_u, pos_v, width, height):
@@ -29,34 +54,25 @@ class PyxelImage:
   def print(self):
     pyxel.blt(self.pos_x, self.pos_y, self.imgnum, self.pos_u, self.pos_v, self.width, self.height)
 
-class PyxelPenguinImage(PyxelImage):
-  def dance_animation(self):
-    if pyxel.frame_count % 15 != 0: return self
-    if self.pos_u == 0:
-      return PyxelPenguinImage(self.pos_x, self.pos_y, self.imgnum, self.pos_u + 16, self.pos_v, self.width, self.height)
-    else:
-      return PyxelPenguinImage(self.pos_x, self.pos_y, self.imgnum, self.pos_u - 16, self.pos_v, self.width, self.height)
 
 class App:
   def __init__(self):
-    pyxel.init(160, 128, title = "Birthday Penguin")
-    pyxel.load("assetes/main.pyxres")
+    pyxel.init(128, 128, title = "Birthday Penguin")
+    pyxel.load("assetes/penguin.pyxres")
 
-    self.title = PyxelRainbowText(50, 50, "Happy Birthday!!", 0)
-    self.penguin = PyxelPenguinImage(72, 60, 0, 0, 0, 16, 16)
+    self.title = PyxelImage(0, 0, 1, 128, 0, 128, 128)
+    self.score = PyxelDoubledText(30, 4, "35", COLOR_ORANGE, COLOR_BROWN)
 
     pyxel.run(self.update, self.draw)
 
   def update(self):
     if pyxel.btnp(pyxel.KEY_Q):
       pyxel.quit()
-    self.title = self.title.roop_color()
-    self.penguin = self.penguin.dance_animation()
 
   def draw(self):
     pyxel.cls(BG_COLOR)
     self.title.print()
-    self.penguin.print()
+    self.score.print()
 
 if __name__ == "__main__":
   App()
