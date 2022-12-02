1040c3a950d3bd3615aa1b3e555fd76ea48aa0bc
UniUrchin <85057870+UniUrchin@users.noreply.github.com>
2022-12-03 07:28:36 +0900

    最低限の機能を備えた

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pyxel
 import random
 
+# Pyxelカラーパレット早見表
 COLOR_BLACK      = 0
 COLOR_NAVY       = 1
 COLOR_PURPLE     = 2
@@ -18,10 +19,18 @@ COLOR_GRAY       = 13
 COLOR_PINK       = 14
 COLOR_PEACH      = 15
 
+# システム変数...のつもり(後でリファクタリングする時に仕分ける)
 SYSTEM_BG_COLOR = COLOR_PEACH
+SYSTEM_TIME_RIMIT = 60
 
+# お菓子の識別ナンバー
 SWEETS_DONUT     = 0
 SWEETS_SHORTCAKE = 1
+
+# ページ遷移用変数
+PAGE_MAIN_MENU     = 0
+PAGE_GAME_SCREEN   = 1
+PAGE_RESULT_SCREEN = 2
 
 class PyxelText:
   def __init__(self, pos_x, pos_y, word, color):
@@ -63,6 +72,10 @@ class Score(PyxelDoubledText):
     pyxel.text(self.pos_x + 1, self.pos_y, str(self.word), self.shadow_color)
     pyxel.text(self.pos_x, self.pos_y, str(self.word), self.main_color)
 
+class Timer(PyxelDoubledText):
+  def print(self):
+    pyxel.text(self.pos_x + 1, self.pos_y, str(self.word).zfill(2), self.shadow_color)
+    pyxel.text(self.pos_x, self.pos_y, str(self.word).zfill(2), self.main_color)
 
 class Penguin(PyxelImage):
   def __init__(self, pos_x, pos_y, pos_u, pos_v):
@@ -93,9 +106,9 @@ class Sweets(PyxelImage):
     self.height = 16
 
   def fall_down(self):
-    if self.pos_u // 16 == 0:
+    if self.pos_u // 16 == SWEETS_DONUT:
       return Sweets(self.pos_x, self.pos_y + 1, self.pos_u, self.pos_v)
-    if self.pos_u // 16 == 1:
+    if self.pos_u // 16 == SWEETS_SHORTCAKE:
       return Sweets(self.pos_x, self.pos_y + 2, self.pos_u, self.pos_v)
 
   def is_inside_screen(self):
@@ -109,62 +122,98 @@ class App:
     pyxel.init(128, 128, title = "Birthday Penguin")
     pyxel.load("assetes/penguin.pyxres")
 
-    self.stage = PyxelImage(0, 0, 1, 128, 0, 128, 128)
-    self.score = Score(30, 4, 0, COLOR_ORANGE, COLOR_BROWN)
+    self.stage = PyxelImage(0, 0, 1, 0, 0, 128, 128)
+    self.score = Score(26, 4, 0, COLOR_ORANGE, COLOR_BROWN)
+    self.timer = Timer(118, 4, SYSTEM_TIME_RIMIT, COLOR_ORANGE, COLOR_BROWN)
     self.penguin = Penguin(64, 106, 0, 0)
     self.sweets_list = []
+    self.screen = PAGE_MAIN_MENU
+    self.start = PyxelImage(26, 84, 1, 0, 128, 20, 8)
+    self.result = PyxelImage(22, 72, 1, 32, 128, 24, 8)
+    self.score_text = PyxelImage(16, 88, 1, 0, 136, 24, 8)
 
     pyxel.run(self.update, self.draw)
 
   def update(self):
-    # キーボード入力処理
-
     ## Qを押すとゲームが強制終了する
     if pyxel.btnp(pyxel.KEY_Q):
       pyxel.quit()
 
-    ## 十字キーの左右でぺんぎんの移動 
-    if pyxel.btn(pyxel.KEY_LEFT):
-      self.penguin = self.penguin.walk_left()
-    if pyxel.btn(pyxel.KEY_RIGHT):
-      self.penguin = self.penguin.walk_right()
-    
-    # タイマー処理(1秒に30フレーム)
+    ## メインメニューでの処理
+    if self.screen == PAGE_MAIN_MENU:
+      ## スペースーキーが押されたら、ゲーム画面に遷移する
+      if pyxel.btn(pyxel.KEY_SPACE):
+        self.screen = PAGE_GAME_SCREEN
+        self.stage = PyxelImage(0, 0, 1, 128, 0, 128, 128)
 
-    ## 2秒(60フレーム)に一回お菓子を生成する処理
-    if pyxel.frame_count % 60 == 0:
-      self.sweets_list.append(Sweets(random.randint(0, pyxel.width - 16), 0, random.randint(0, 1) * 16, 32))
+    ## ゲーム画面での処理
+    if self.screen == PAGE_GAME_SCREEN:
+      ## 十字キーの左右でぺんぎんの移動 
+      if pyxel.btn(pyxel.KEY_LEFT):
+        self.penguin = self.penguin.walk_left()
+      if pyxel.btn(pyxel.KEY_RIGHT):
+        self.penguin = self.penguin.walk_right()
 
-    # 毎フレームやりたい処理
+      ## 1秒に一回タイマーを進める処理
+      if pyxel.frame_count % 30 == 0:
+        self.timer = Timer(118, 4, self.timer.word - 1, COLOR_ORANGE, COLOR_BROWN)
 
-    ## お菓子の移動とスクリーン外に出たお菓子の廃棄
-    self.sweets_list = list(map(lambda sweets: sweets.fall_down(), self.sweets_list))
-    self.sweets_list = list(filter(lambda sweets: sweets.is_inside_screen(), self.sweets_list))
+      ## 2秒に一回お菓子を生成する処理
+      if pyxel.frame_count % 60 == 0:
+        self.sweets_list.append(Sweets(random.randint(0, pyxel.width - 16), 0, random.randint(0, 1) * 16, 32))
 
-    # お菓子の接触判定(スコアの加算処理もやる)
-    self.sweets_list = list(filter(lambda sweets: self.update_score(sweets), self.sweets_list))
+      ## お菓子の移動とスクリーン外に出たお菓子の廃棄
+      self.sweets_list = list(map(lambda sweets: sweets.fall_down(), self.sweets_list))
+      self.sweets_list = list(filter(lambda sweets: sweets.is_inside_screen(), self.sweets_list))
+
+      # お菓子の接触判定(スコアの加算処理もやる)
+      self.sweets_list = list(filter(lambda sweets: self.update_score(sweets), self.sweets_list))
+
+      ## タイマーがゼロになったら、リザルト画面へ遷移
+      if self.timer.word == 0:
+        self.screen = PAGE_RESULT_SCREEN
+        self.stage = PyxelImage(0, 0, 1, 0, 0, 128, 128)
+        self.score = Score(40, 88, self.score.word, COLOR_ORANGE, COLOR_BROWN)
+        self.timer = Timer(118, 4, SYSTEM_TIME_RIMIT, COLOR_ORANGE, COLOR_BROWN)
+        self.penguin = Penguin(64, 106, 0, 0)
+        self.sweets_list = []
+
+    # リザルト画面での処理
+    if self.screen == PAGE_RESULT_SCREEN:
+      ## スペースキーが離れたら、メインメニューに戻る(何故かこうしないと勝手に戻ってしまう...)
+      if pyxel.btnr(pyxel.KEY_SPACE):
+        self.screen = PAGE_MAIN_MENU
+        self.score = Score(26, 4, 0, COLOR_ORANGE, COLOR_BROWN)
 
   def update_score(self, sweets):
     if sweets.is_touched(self.penguin.pos_x, self.penguin.pos_y):
-      if sweets.pos_u // 16 == 0:
-        self.score = Score(30, 4, self.score.word + 100, COLOR_ORANGE, COLOR_BROWN)
-      if sweets.pos_u // 16 == 1:
-        self.score = Score(30, 4, self.score.word + 200, COLOR_ORANGE, COLOR_BROWN)
+      if sweets.pos_u // 16 == SWEETS_DONUT:
+        self.score = Score(26, 4, self.score.word + 100, COLOR_ORANGE, COLOR_BROWN)
+      if sweets.pos_u // 16 == SWEETS_SHORTCAKE:
+        self.score = Score(26, 4, self.score.word + 200, COLOR_ORANGE, COLOR_BROWN)
       return False
     else:
       return True
 
   def draw(self):
     pyxel.cls(SYSTEM_BG_COLOR)
-
+    
     self.stage.print()
 
-    for sweets in self.sweets_list:
-      sweets.print()
+    if self.screen == PAGE_MAIN_MENU:
+      self.start.print()
 
-    self.score.print()
-    self.penguin.print()
+    if self.screen == PAGE_GAME_SCREEN:
+      for sweets in self.sweets_list:
+        sweets.print()
+      self.score.print()
+      self.timer.print()
+      self.penguin.print()
 
+    if self.screen == PAGE_RESULT_SCREEN:
+      self.result.print()
+      self.score.print()
+      self.score_text.print()
 
 if __name__ == "__main__":
   App()
